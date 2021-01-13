b5374861ebe7aeb8646ea6e09eff6982dd88900d
santosbpm <santosbpm.0@gmail.com>
2021-01-13 12:34:28 -0300

    Adicioando as funcoes InserirConta e ListarConta

--- Program.cs
+++ Program.cs
@@ -1,21 +1,108 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace bank_account
 {
     class Program
     {
+
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Marcos");
-            Console.WriteLine(minhaConta.ToString());
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						ListarContas();
+						break;
+					case "2":
+						InserirConta();
+						break;
+					case "3":
+						Transferir();
+						break;
+					case "4":
+						Sacar();
+						break;
+					case "5":
+						Depositar();
+						break;
+                    case "C":
+						Console.Clear();
+						break;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+
+			}
+        }
+
+        private static void Depositar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Sacar()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Transferir()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta!");
+
+            Console.WriteLine("Digite 1 para Conta Fisica ou 2 para Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.WriteLine("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+    
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+
+            listContas.Add(novaConta);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+            
+            if(listContas.Count == 0) {
+                Console.WriteLine("Nenhuum conta cadastrada.");
+                return;
+            }
+            
+            for(int i = 0; i < listContas.Count; i++) {
+                Conta conta = listContas[i];
+                Console.Write($"#{i} - ");
+                Console.WriteLine(conta);
+            }
         }
 
         private static string ObterOpcaoUsuario()
 		{
 			Console.WriteLine();
-			Console.WriteLine("DIO Bank a seu dispor!!!");
+			Console.WriteLine("Bank a seu dispor!!!");
 			Console.WriteLine("Informe a opção desejada:");
-
+            Console.WriteLine();
 			Console.WriteLine("1- Listar contas");
 			Console.WriteLine("2- Inserir nova conta");
 			Console.WriteLine("3- Transferir");
@@ -24,7 +111,6 @@ namespace bank_account
             Console.WriteLine("C- Limpar Tela");
 			Console.WriteLine("X- Sair");
 			Console.WriteLine();
-
 			string opcaoUsuario = Console.ReadLine().ToUpper();
 			Console.WriteLine();
 			return opcaoUsuario;
