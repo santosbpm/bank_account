624209bac0f68fcbcf7054dca6668a3bab753b09
santosbpm <santosbpm.0@gmail.com>
2021-01-13 11:20:50 -0300

    Criando base do projeto

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+namespace bank_account
+{
+    public enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bank_account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta.Conta();
+        }
+    }
+}
