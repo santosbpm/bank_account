0d06732d05e5309992dc33e4758a842f4c0d485f
santosbpm <santosbpm.0@gmail.com>
2021-01-13 12:00:33 -0300

    Adicionando um menu como interface de interacao para o usuario

--- Program.cs
+++ Program.cs
@@ -7,8 +7,27 @@ namespace bank_account
         static void Main(string[] args)
         {
             Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "Marcos");
-
             Console.WriteLine(minhaConta.ToString());
         }
+
+        private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("DIO Bank a seu dispor!!!");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar contas");
+			Console.WriteLine("2- Inserir nova conta");
+			Console.WriteLine("3- Transferir");
+			Console.WriteLine("4- Sacar");
+			Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
     }
 }
